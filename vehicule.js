39effce33900f72fc1edeeb05794bed0348a2c1a
Selbahc <selbahc@users.noreply.github.comm>
2017-06-13 13:32:54 +0200

    Ajout de 4 autres vehicules

--- vehicule.js
+++ vehicule.js
@@ -7,8 +7,8 @@ function Vehicule(modele, km, premium, moteur, couleur, voyage) {
     this.moteur = moteur;
     this.couleur = couleur;
   } else {
-    this.moteur = "Essence";
-    this.couleur = "Blanche";
+    this.moteur = "essence";
+    this.couleur = "blanche";
   }
 
   this.voyage = function(km) {
@@ -19,4 +19,21 @@ function Vehicule(modele, km, premium, moteur, couleur, voyage) {
 
 var tesla = new Vehicule("Tesla Model S", 0, "premium", "electrique", "noire");
 tesla.voyage(335);
+
+var peugeot107 = new Vehicule("Peugeot 107", 123000, "non");
+peugeot107.voyage(545);
+peugeot107.voyage(523);
+
+var toyotaPrius = new Vehicule("Toyota Prius", 22000, "oui", "hybride", "grise");
+
+var bmwI3 = new Vehicule("BMW i3", 0, true, "electrique", "noire et blanche");
+bmwI3.voyage(246);
+
+var renaultEspace = new Vehicule("Renault Espace", 80037, false);
+
+
 console.log(tesla);
+console.log(peugeot107);
+console.log(toyotaPrius);
+console.log(bmwI3);
+console.log(renaultEspace);
